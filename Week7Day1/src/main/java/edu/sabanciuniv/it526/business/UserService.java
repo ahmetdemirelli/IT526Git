2f6041b5a80ce4b8eb5929e8d2e9fae42fb59ccd
Ahmet Demirelli <ahmetdemirelli@sabanciuniv.edu>
2021-04-24 09:20:48 +0300

    First Commit

---
+++ Week7Day1/src/main/java/edu/sabanciuniv/it526/business/UserService.java
@@ -0,0 +1,37 @@
+package edu.sabanciuniv.it526.business;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.jws.soap.SOAPBinding.Use;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import edu.sabanciuniv.it526.domain.User;
+import edu.sabanciuniv.it526.utils.IT526Utillty;
+
+@Stateless
+public class UserService {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	public User checkUser(String username, String password) {
+		
+		password = IT526Utillty.hash(password);
+		System.out.println("User Input password (from textbox) hashed --> "+password);
+		
+		 List<User> users = entityManager.createQuery("select u from User u where u.username=?1 and u.password=?2",User.class)
+				 .setParameter(1, username)
+				 .setParameter(2, password)
+				 .getResultList();	
+		if (users.size()==1)
+		{
+			System.out.println("User password ( from DB ) hashed --> "+password);
+			System.out.println("User Exists");
+			return users.get(0);
+		}
+		return null;
+	}
+
+}
